fc18d8d98aa88d9ba62d9b352dc2fdf7b24e810c
betta18 <jiangmengyu1@huawei.com>
2026-04-13 16:38:51 +0800

    [Feature][NPU] support hyimage3 offline quant by vllm-ascend.
    Signed-off-by: betta18 <jiangmengyu1@huawei.com>

--- vllm_omni/platforms/interface.py
+++ vllm_omni/platforms/interface.py
@@ -5,6 +5,7 @@ from enum import Enum
 from typing import Any
 
 import torch
+import torch.nn as nn
 from vllm.platforms import Platform
 
 
@@ -61,6 +62,13 @@ class OmniPlatform(Platform):
 
     @classmethod
     def prepare_diffusion_op_runtime(cls, op_name: str, **kwargs: Any) -> None:
+        return None
+
+    @classmethod
+    def get_diffusion_packed_modules_mapping(
+        cls,
+        model_class: type[nn.Module],
+    ) -> dict[str, list[str]] | None:
         return None
 
     @classmethod
